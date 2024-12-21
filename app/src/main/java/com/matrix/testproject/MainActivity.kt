e6ad5cfc772d614001661940a31061b8aceee75c
saurav <sauravsajeev202@gmail.com>
2024-12-21 22:04:02 +0530

    demo

--- app/src/main/java/com/matrix/testproject/MainActivity.kt
+++ app/src/main/java/com/matrix/testproject/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.matrix.testproject
 
 import android.os.Bundle
+import android.text.InputType
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.matrix.otpview.OtpView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val otpView = findViewById<OtpView>(R.id.otp)
+        otpView.apply {
+            setInputType(InputType.TYPE_CLASS_TEXT)
+            setSquareCount(6)
+            setOnCompleteListener { it ->
+                if (!checkOTP(it))
+                    onOtpError(true)
+            }
+        }
     }
+
+    fun checkOTP(otp: String): Boolean {
+        return otp == "123456"
+    }
+
 }
